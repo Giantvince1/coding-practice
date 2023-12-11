46a11149b712b0415b5a0d0314c8f149dfe99642
Giantvince1 <giantvince1@protonmail.com>
2023-12-11 10:05:15 -0600

    Rename test.cpp to test.c, add gitignore, update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # coding-practice
-This is my public repository holding all my coding practice material whilst beggining to learn how to code in C and other languages.
+This is my public repository holding all my coding practice material whilst beggining to learn how to code.
+I'm starting with C, C++, Python, POSIX SH, and Microsoft PowerShell Core (the cross-platform version).
+
+All code pushed to this repository is freely available to be used by anyone, provided they follow the GPLv3.
+As of now, these programs just show very basic examples of the language structure in use.
---
+++ c/fibcounter/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) {
+    unsigned long long int a = 1;
+    unsigned long long int b = 1;
+    unsigned long long int x;
+    unsigned long long int maxint = 18446744073709551615;
+    x = a + b;
+    printf("%llu\n", a);
+    printf("%llu\n", b);
+    printf("%llu\n", x);
+    unsigned long long int max = 1000;
+    while (x < max) {
+        a = b;
+        b = x;
+        x = a + b;
+        if (x > max) {
+            printf("Done! Last number in sequence: %llu%s%llu\n", b, ", max number to look up to: ", max);
+        }
+        else {
+            printf("%llu\n", x);
+        }
+    }
+}
